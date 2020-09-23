1d3adcd7ecaf6d7a762adb10a04511b6304028f5
Marco <mnicotra@remedia.it>
2020-09-23 15:47:13 +0200

    Replacing debugPrint with dart:developer
    Using dart:developer .log to print on console.

--- lib/core/misslog.dart
+++ lib/core/misslog.dart
@@ -1,6 +1,7 @@
 library versioning;
 
 import 'package:ansicolor/ansicolor.dart';
+import 'dart:developer' as dev;
 
 enum LogType{
   debug,
@@ -12,7 +13,10 @@ enum LogType{
 class MissLog{
 
   static final levelIcon = [
-    '👾','💡','⚠️','⛔',
+    '👾', // debug
+    '💡', // info
+    '🚧', // warning
+    '🔥', // error ⛔
   ];
 
   static const topLeftCorner = '┌';
@@ -26,10 +30,16 @@ class MissLog{
   static e(String message, {String tag}) => _log(message, logType: LogType.error, tag: tag);
   static d(String message, {String tag}) => _log(message, logType: LogType.debug, tag: tag);
   static w(String message, {String tag}) => _log(message, logType: LogType.warning, tag: tag);
+
+  static void _printWrapped(String text, currentPen) {
+    final pattern = RegExp('.{1,800}'); // 800 is the size of each chunk
+    pattern.allMatches(text).forEach((match) => dev.log(currentPen(match.group(0)), name: 'MissLog'));
+  } 
     
   static void _log(String message, {
     String tag,
     LogType logType=LogType.debug,
+    bool hideTime=false,
   }){
 
     bool _isDisabled = true;
@@ -50,15 +60,15 @@ class MissLog{
     switch(logType){
       case LogType.debug:
         icon = levelIcon[0];
-        currentPen = AnsiPen()..green();
+        currentPen = AnsiPen()..green(bold: false);
         break;
       case LogType.info:
         icon = levelIcon[1];
-        currentPen = AnsiPen()..blue();
+        currentPen = AnsiPen()..blue(bold: false);
         break;
       case LogType.warning:
         icon = levelIcon[2];
-        currentPen = AnsiPen()..cyan();
+        currentPen = AnsiPen()..yellow();
         break;
       case LogType.error:
         icon = levelIcon[3];
@@ -69,14 +79,14 @@ class MissLog{
         icon = levelIcon[0];
     }
 
-    void printWrapped(String text) {
-      final pattern = RegExp('.{1,800}'); // 800 is the size of each chunk
-      pattern.allMatches(text).forEach((match) => print(currentPen(match.group(0))));
-    } 
+    String when = '';
+    if(!hideTime) 
+      when = DateTime.now().toIso8601String().split('T')[1];
 
-    print(currentPen('$topLeftCorner────────────────────────────────────────'));
-    printWrapped(currentPen(' $icon MissLog ==> $tagger: $message'));
-    print(currentPen('$bottomLeftCorner────────────────────────────────────────'));
+    dev.log(currentPen('$topLeftCorner──────────────────────────────────────── ' + when), name: 'MissLog',);
+    dev.log(currentPen(' $icon ==> $tagger: $message'), name: 'MissLog');
+    // _printWrapped(currentPen(' $icon ==> $tagger: $message'), currentPen);
+    dev.log(currentPen('$bottomLeftCorner────────────────────────────────────────'), name: 'MissLog');
 
   }
 
